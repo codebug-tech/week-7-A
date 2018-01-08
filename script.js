9298c66ff2e3294a7b1bd6ae70d757cb0ac92b94
mcarthurgill <mcarthur.gill@gmail.com>
2018-01-07 19:35:17 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,122 @@
+document.addEventListener('DOMContentLoaded', function() {
+//////////////////////////////////////////////////////////////////////////
+  // Write the `helloWorld` function below.
+
+
+
+  // Uncomment the line below, and look in the console to see if your function worked!
+  // console.log(helloWorld())
+  // Expected output: Hello World!
+
+//////////////////////////////////////////////////////////////////////////
+  // Write `yell` below. 
+
+
+
+  // Uncomment the line below, and look in the console to see if your function worked!
+  // console.log(yell("Party!"))
+  // Expected output: PARTY!
+
+//////////////////////////////////////////////////////////////////////////
+  // Write `makeMoreExciting` below.
+
+
+
+  // Uncomment the line below, and look in the console to see if your function worked!
+  // console.log(makeMoreExciting("It's Tuesday"));
+  // Expected output: It's Tuesday!!!
+
+//////////////////////////////////////////////////////////////////////////
+  // Write `getLength` below.
+
+
+
+
+  // Uncomment the line below, and look in the console to see if your function worked!
+  // console.log(getLength("How long am I? Too long? Too short?"));
+  // Expected output: 35
+
+//////////////////////////////////////////////////////////////////////////
+  // Write `rockstarNamer` below.
+
+
+
+  // Uncomment the line below, and look in the console to see if your function worked!
+  // console.log(rockstarNamer("Snickers", "Cloverdale"));
+  // Expected output: Your rockstar name is Snickers Cloverdale.
+
+//////////////////////////////////////////////////////////////////////////
+  // Write `combinedLength` below.
+
+
+
+
+  // Uncomment the lines below, and look in the console to see if your function worked!
+  // console.log(combinedLength("Blueberry", "Muffins are my homies"));
+  // Expected output: 30
+
+//////////////////////////////////////////////////////////////////////////
+  // Write `bigDaddy` below.
+
+
+
+
+  // Uncomment the lines below, and look in the console to see if your function worked!
+  // console.log(bigDaddy(17, 10));
+  // Expected output: 17
+  // console.log(bigDaddy(100, 200));
+  // Expected output: 200
+  // console.log(bigDaddy(-4, 4));
+  // Expected output: 4
+  // console.log(bigDaddy(21, 21));
+  // Expected output: 21
+  
+//////////////////////////////////////////////////////////////////////////
+  // Write `babyDaddy` below.
+
+
+
+
+  // Uncomment the line below, and look in the console to see if your function worked!
+  // console.log(babyDaddy(17, 10));
+  // Expected output: 10
+  // console.log(babyDaddy(100, 200));
+  // Expected output: 100
+  // console.log(babyDaddy(-4, 4));
+  // Expected output: -4
+  // console.log(babyDaddy(21, 21));
+  // Expected output: 21
+
+//////////////////////////////////////////////////////////////////////////
+  // Write `breakfastChef` below.
+
+
+
+
+  // Uncomment the lines below, and look in the console to see if your function worked!
+  // console.log(breakfastChef(true, true));
+  // Expected output: "chicken and waffles"
+  // console.log(breakfastChef(true, false));
+  // Expected output: "ham and cheese omelette"
+  // console.log(breakfastChef(false, true));
+  // Expected output: "chocolate chip pancakes"
+  // console.log(breakfastChef(false, false));
+  // Expected output: "water"
+
+//////////////////////////////////////////////////////////////////////////
+  // Write `tweener` below.
+
+
+
+
+  // Uncomment the lines below, and look in the console to see if your function worked!
+  // console.log(tweener(1, 4));
+  // Expected output: Your output should be between 1 and 4
+  // console.log(tweener(30, 2));
+  // Expected output: Your output should be between 2 and 30
+  // console.log(tweener(-10, -4));
+  // Expected output: Your output should be between -4 and -10
+
+//////////////////////////////////////////////////////////////////////////
+
+})
